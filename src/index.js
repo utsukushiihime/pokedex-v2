a47c410143fca9ceddb6f1ce13c0085529128166
Crystal McNeil <crystalsmcneil@gmail.com>
2022-08-17 18:29:04 -0600

    init commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import {
+	ApolloClient,
+	InMemoryCache,
+	ApolloProvider,
+	gql,
+} from '@apollo/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const client = new ApolloClient({
+	uri: 'https://beta.pokeapi.co/graphql/v1beta',
+	cache: new InMemoryCache(),
+});
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+client
+	.query({
+		query: gql`
+			query samplePokeAPIquery {
+				# Gets all the pokemon belonging to generation 3
+				gen3_species: pokemon_v2_pokemonspecies(
+					where: {
+						pokemon_v2_generation: {
+							name: { _eq: "generation-iii" }
+						}
+					}
+					order_by: { id: asc }
+				) {
+					name
+					id
+				}
+
+				# You can run multiple queries at the same time
+				# Counts how many pokemon were released for each generation
+				generations: pokemon_v2_generation {
+					name
+					pokemon_species: pokemon_v2_pokemonspecies_aggregate {
+						aggregate {
+							count
+						}
+					}
+				}
+			}
+		`,
+	})
+	.then((result) => console.log(result));
+
+root.render(
+	<ApolloProvider client={client}>
+		<App />
+	</ApolloProvider>
+);
